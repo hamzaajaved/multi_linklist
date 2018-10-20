f13e3bbf08b93b7fba978707232ff9e9415cf1be
Hamza <hamzajaved091@gmail.com>
2018-10-20 14:47:03 -0700

    insert and print using address

--- main.cpp
+++ main.cpp
@@ -17,36 +17,37 @@ struct Student{
   double GP;
   struct Student *next;
 };
-struct CLASS *start = NULL;
-void insert_class(string Name,int no_of_std);
-void insert_student(string Name,int seat_no,double GP);
-void print_class();
-void print_student();
+
+void insert_class(string Name,int no_of_std,CLASS **start);
+void insert_student(string Name,int seat_no,double GP,Student **start);
+void print_class(CLASS **start);
 int main(int argc, char const *argv[])
 {
-  insert_class("I",10);
-  insert_class("II",20);
-  insert_student("Hamza",90,3.0);
-  insert_student("Hadi",164,3.9);
+  struct CLASS *start = NULL;
+  insert_class("I",10, &start);
+  insert_class("II",20,&start);
+  insert_class("III",10,&start);
+  insert_student("Hamza",90,3.0,&start->header);
+  insert_student("Hadi",164,3.9,&start->header);
 
-  print_class();
+  print_class(&start);
 
   system("pause");
   return 0;
 }
 
 // INSERT CLASS
-void insert_class(string Name,int no_of_std){
+void insert_class(string Name,int no_of_std,CLASS **start){
   struct CLASS *ptr = new CLASS;
   ptr->name = Name;
   ptr->no_of_std = no_of_std;
   ptr->next = NULL;
   ptr->header = NULL;
 
-  if(start == NULL){
-    start = ptr;
+  if(*start == NULL){
+    *start = ptr;
   }else{
-    struct CLASS *curr = start;
+    struct CLASS *curr = *start;
     while(curr->next != NULL){
       curr = curr->next;
     }
@@ -55,17 +56,17 @@ void insert_class(string Name,int no_of_std){
 }
 
 // INSERT STUDENT
-void insert_student(string Name,int seat_no,double GP){
+void insert_student(string Name,int seat_no,double GP,Student **start){
   struct Student *ptr = new Student;
   ptr->name = Name;
   ptr->seat_no = seat_no;
   ptr->GP = GP;
   ptr->next = NULL;
 
-  if(start->header == NULL){
-    start->header = ptr;
+  if(*start == NULL){
+    *start = ptr;
   }else{
-    struct Student *curr = start->header;
+    struct Student *curr = *start;
     while(curr->next != NULL){
       curr = curr->next;
     }
@@ -74,8 +75,8 @@ void insert_student(string Name,int seat_no,double GP){
 }
 
 //PRINT CLASS
-void print_class(){
-  struct CLASS *currClass = start;
+void print_class(CLASS **start){
+  struct CLASS *currClass = *start;
   while(currClass != NULL){
     cout << "\n\nThe Student in Class " << currClass->name << " are: " << endl;
     struct Student *currStudent = currClass->header;
